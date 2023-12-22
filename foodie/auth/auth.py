765519305863638f8ebf51b980b6e22568265024
Samuel Ogboye <ogboyesam@gmail.com>
2023-12-22 03:12:32 +0100

    implementing Google Oauth

--- foodie/auth/auth.py
+++ foodie/auth/auth.py
@@ -385,3 +385,99 @@ def make_admin(user, user_id):
     user.is_admin = True
     db.session.commit()
     return jsonify(user.format()), 200
+
+import os
+import json
+import requests
+from oauthlib.oauth2 import WebApplicationClient
+
+# Configuration
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+# OAuth2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+# Endpoint for Google login
+@auth_bp.route("/google", methods=["GET"])
+def google_login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    print("Request URI:", request_uri)
+    return jsonify({"redirect": request_uri})
+
+# Endpoint for Google login callback
+@auth_bp.route("/google/callback", methods=["GET"])
+def google_callback():
+    print("Google callback")
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send request to get tokens
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Get user info from Google
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # Check if user email is verified
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return jsonify({"error": "User email not available or not verified by Google."}), 400
+
+    # Create a user with the info provided by Google
+    user = User.query.filter_by(email=users_email).first()
+    if not user:
+        user = User(
+            unique_id=unique_id,
+            email=users_email,
+            name=users_name,
+            picture=picture
+        )
+        db.session.add(user)
+        db.session.commit()
+    # Login the user by returning them access token
+    access_token = create_access_token(identity=user.id)
+    refresh_token = create_refresh_token(identity=user.id)
+    response = jsonify({"msg": "login successful", "accessToken": access_token})
+
+
